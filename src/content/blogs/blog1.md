457c01775b8cedf8bd03fb3b6bc793fb75f1b42b
Ayush kumar <kumarayushkumar@protonmail.com>
2024-12-08 16:20:44 +0530

    Replace img with Image component in Blog and Trending pages, update blog image paths, and enhance content schema

--- src/content/blogs/blog1.md
+++ src/content/blogs/blog1.md
@@ -1,7 +1,7 @@
 ---
 title: 'Blog 1'
 date: '2021-01-01'
-image: '/src/images/blogs/test.png'
+image: ./images/h.png
 content: 'This is the first blog post.'
 keywords: 'blog, first'
 description: 'This is the first blog post.'
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,15 @@
 import { defineCollection, z } from 'astro:content'
 
 const blogs = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    keywords: z.string(),
-    description: z.string(),
-    date: z.string(),
-    image: z.string(),
-    content: z.string()
-  })
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      keywords: z.string(),
+      description: z.string(),
+      date: z.string(),
+      image: image(),
+      content: z.string()
+    })
 })
 
 export const collections = {
